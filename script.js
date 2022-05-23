1a8c0159becdf933b68ef4073ec9b2f1eee1db79
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-05-23 10:03:10 +0300

    test Arrays

---
+++ script.js
@@ -0,0 +1,12 @@
+var arr = ["a", "b", "c"];
+var newItem ="d";
+
+console.log(arr);
+
+function push(arr,newItem){
+    arr[arr.length]=newItem;
+    return arr;
+}
+console.log(push(arr,newItem))
+console.log(push(arr,"newItemx"))
+console.log(push(arr))
