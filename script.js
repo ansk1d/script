a943b92731a2344850170907ed29536d811d1f11
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-05-23 10:12:36 +0300

    clean

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var arr = ["a", "b", "c"];
 
-console.log(arr);
-
 function push(arr, newItem) {
     arr[arr.length] = newItem;
     return arr;
 }
-console.log(push(arr,"d"));
+console.log(push(arr, "d"));
