2898299e7d37eb3191d996a7e71980542b67fd23
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-05-23 10:06:17 +0300

    nothin

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var arr = ["a", "b", "c"];
-var newItem ="d";
+var newItem = "d";
 
 console.log(arr);
 
-function push(arr,newItem){
-    arr[arr.length]=newItem;
+function push(arr, newItem) {
+    arr[arr.length] = newItem;
     return arr;
 }
-console.log(push(arr,newItem))
-console.log(push(arr,"newItemx"))
+console.log(push(arr, newItem))
+console.log(push(arr, "newItemx"))
 console.log(push(arr))
