419e1139da0ab0b0e8f9a626bcf35bb72508aa88
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-05-23 10:20:11 +0300

    Fainal

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var arr = ["a", "b", "c"];
 let newItem = "d"
+
 function push(arr, newItem) {
     arr[arr.length] = newItem;
     return arr;
 }
-push(arr,newItem)
+
+push(arr, newItem);
 console.log(arr);
